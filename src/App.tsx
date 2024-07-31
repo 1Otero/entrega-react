0190e32c5f0c2ff44615e77ab0b14a48a3fb2e30
van_da <iivnnn123@gmail.com>
2024-07-31 17:55:32 -0500

    edir error

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
 import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import { version } from 'react'
 import Pago from './components/pago/Pago.tsx'
 import { Routes, Route } from 'react-router-dom'
 import Products from './components/product/Product.tsx'
