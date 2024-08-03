2a9ccaf64337bef383321d0026a03efd7e474a35
van_da <iivnnn123@gmail.com>
2024-08-03 03:23:27 -0500

    mejora texto

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -6,7 +6,6 @@ function card({cardProduct, setLoad, setAlertGlobal, setShowCart, setProductCard
   const [infoTarjeta, setInfoTarjeta]= useState<{toktarjeta: String, acceptConfirm: String}>(Object)
   const [ver, setVer]= useState(false)
   function comprar(){
-    console.log("ffffffff")
     setVer(!ver)
   }
   return <>
@@ -40,7 +39,7 @@ function card({cardProduct, setLoad, setAlertGlobal, setShowCart, setProductCard
     {ver?<p>
         <Pago infoTarjeta={infoTarjeta} setInfoTarjeta={setInfoTarjeta} infoCarrito={cardProduct} setLoad={setLoad} setAlertGlobal={setAlertGlobal} setShowCart={setShowCart} setProductCard={setProductCard}/>
     </p>:<div></div>}
-    {cardProduct.length>0?<button className="m-2 inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={comprar}>Validar compra</button>:<div></div>} 
+    {cardProduct.length>0?<button className="m-2 inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={comprar}>{ !ver ? <p>Validar compra</p> : <p>Ocultar compra</p> }</button>:<div></div>} 
     {/* <button onClick={finalizarPago}>Pagar</button> */}
   </>
 }
