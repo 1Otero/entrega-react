58b9f1cd4d39270ea099a2102535f08b2bd453f8
van_da <iivnnn123@gmail.com>
2024-08-03 02:36:35 -0500

    lol

--- src/components/pago/sendPago.tsx
+++ src/components/pago/sendPago.tsx
@@ -1,35 +1,33 @@
 import axios from "axios"
 import u from '../utils/env/env'
 import pay from "../../model/pago/pago"
- 
-//function sendPago(tokenAccept:String, tokenTarjeta:String){
+
 async function sendPago(_pay:pay){
   const {acceptance_token, payment_method, amount_in_cents, infoCarrito, currency, customer_email, customer_data, signature, reference}= _pay
-  const token= payment_method.token
-    const bodyPay= await axios.post(`${u.API}/pago/sendmepago`, {   
-      acceptance_token,
-      infoCarrito,
-      reference,
-      amount_in_cents,
-      currency,
-      signature,
-      // "payment_method": {
-      //  "type": "CARD",
-      //  "installments": 1, // Número de cuotas
-      //  token
-      // },
-      payment_method,
-      customer_email,
-      customer_data
-      // "customer_data": {
-      //   "legal_id": "20121221",
-      //   "phone_number": "+573145678901",
-      //   "full_name": "Ivan Otero (Van)",
-      //   "legal_id_type": "CC"
-      // }
-    })
-    if(!bodyPay) return null 
-    const pay= bodyPay?.data
-    return pay
+  const bodyPay= await axios.post(`${u.API}/pago/sendmepago`, {   
+    acceptance_token,
+    infoCarrito,
+    reference,
+    amount_in_cents,
+    currency,
+    signature,
+    // "payment_method": {
+    //  "type": "CARD",
+    //  "installments": 1, // Número de cuotas
+    //  token
+    // },
+    payment_method,
+    customer_email,
+    customer_data
+    // "customer_data": {
+    //   "legal_id": "20121221",
+    //   "phone_number": "+573145678901",
+    //   "full_name": "Ivan Otero (Van)",
+    //   "legal_id_type": "CC"
+    // }
+  })
+  if(!bodyPay) return null 
+  const pay= bodyPay?.data
+  return pay
 }
 export default sendPago
