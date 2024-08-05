14566633c564cdf534f397165c2225ee980973d8
van_da <iivnnn123@gmail.com>
2024-08-05 04:15:24 -0500

    cambio

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
   const [showCart, setShowCart]= useState(false)
   const [productCard, setProductCard]= useState<Product[]>([])
   const [alertGlobal, setAlertGlobal]= useState<{status: Boolean, message: String, type: String}>({status: false, message: '', type: ''})
-  const [viewTransation, setViewTransation]= useState(true)
+  const [viewTransation, setViewTransation]= useState(false)
   
   return (
     <>
