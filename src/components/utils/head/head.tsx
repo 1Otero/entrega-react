48c0670ef931ea71bb46e0a6ad1312c53f80e419
van_da <iivnnn123@gmail.com>
2024-10-20 14:23:58 -0500

    add comment

--- src/components/utils/head/head.tsx
+++ src/components/utils/head/head.tsx
@@ -2,6 +2,7 @@ const head= () => {
 return (
 <div className="w-full">
 <nav className="bg-gray-200 border-gray-200 dark:bg-gray-900">
+{/* <div className="max-w-screen-xl flex flex-wrap items-center mx-auto p-4"> */}
   <div className="max-w-screen-xl flex flex-wrap items-center justify-center mx-auto p-4">
     <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
         <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">CompraYa</span>
